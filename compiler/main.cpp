d0f39ddfb26f6cda551bb6205e5d257718eb25ab
James Humble <jammiesprideful@gmail.com>
2019-02-08 21:51:34 -0700

    Add CMake Build ability.
    Add the minimal information necessary to each file in order to get cmake to produce all of the build files.
    
    Note: make does not work.

---
+++ compiler/main.cpp
@@ -0,0 +1,8 @@
+#include "symbol_table.hpp"
+
+extern int yyparse();
+
+int main()
+{
+  yyparse();
+};
---
+++ compiler/symbol_table.hpp
@@ -0,0 +1,9 @@
+#ifndef SYMBOL_TABLE_HPP
+#define SYMBOL_TABLE_HPP
+
+#include <string>
+#include <vector>
+#include <map>
+
+
+#endif
